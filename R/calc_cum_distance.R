53811755edb29122073889c55dc8bd6899f0c6cf
Sage <sage@MacBook-Air.local>
2017-12-09 15:00:24 -0700

    updated ref. to geosphere

--- R/calc_cum_distance.R
+++ R/calc_cum_distance.R
@@ -3,7 +3,7 @@ calc_cum_distance <- function(lon, lat) {
     if (i == 1) return(0)
     prev_coord <- c(lon[i-1], lat[i-1])
     curr_coord <- c(lon[i], lat[i]) 
-    return(geosphere::distm(prev_coord, curr_coord, fun = distHaversine))
+    return(geosphere::distm(prev_coord, curr_coord, fun = geosphere::distHaversine))
   }, double(1)) -> dist_btwn_pts
   return(cumsum(dist_btwn_pts))
 }
